220ec88272174f21936854dd8409dd77cf7fbd94
Atchara  Pathon <austchara9730@gmail.com>
2024-08-17 18:35:17 +0700

    2-calculator.js and App.js
    2-calculator.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+/* ข้อ1
 import React from 'react'
 import Button from './class-components'
 function App(){
   return <Button/>
 }
-export default App
+export default App*/
+
+import React from 'react'
+import {Calculator2} from './calculator'
+
+export default function App(){
+    return<Calculator2/>
+}
