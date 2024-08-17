0d41a359f488d56752fca8a454986bb613ad2035
Atchara  Pathon <austchara9730@gmail.com>
2024-08-17 17:28:32 +0700

    this in Class Component
    App.js and class-component.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from 'react'
-import Calendar from './class-components'
-import { Header,Content,Footer} from './func-components'
-
+import Button from './class-components'
 function App(){
-  return(
-    <>
-    <Header/>
-    <p><center><Calendar/></center></p>
-    <Content/>
-    <Footer/>
-    </>
-  )
+  return <Button/>
 }
 export default App
--- src/class-components.js
+++ src/class-components.js
@@ -1,19 +1,14 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
-export default class Calendar extends Component{//สืบทอดตรงๆ
-    getData(){
-        const dayNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-        const monthNames =['January','February','March','April','May','June','July','August',
-            'September','October','November','December']
-        const date =new Date()
-        const weekDay = dayNames[date.getDay()]//ระบุindex
-        const day = date.getDate()
-        const month = monthNames[date.getMonth()]
-        const year = date.getFullYear()
-        return `${weekDay} ${day} ${month} ${year}` //alt+96 เพราะเอาตัวแปรมาแทรก
+export default class Button extends Component{//สืบทอดตรงๆ
+    showAlert(msg){
+      alert(msg)
+    }
+    onClickButtonOK =()=>{
+      this.showAlert('Hello')
     }
     render(){
-        return <div>{this.getData()}</div>
+        return <button onClick={this.onClickButtonOK}>OK</button>
     }
 
 }
